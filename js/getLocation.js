364218d9fd6f854f62fc742e0ba87d3e7dce9b21
talalzeini <56060993+talalzeini@users.noreply.github.com>
2020-11-27 05:24:09 -0800

    testing getLocation.js

--- js/getLocation.js
+++ js/getLocation.js
@@ -1,6 +1,5 @@
-
-
-(function () {
+                                  
+  (function () {
     navigator.geolocation.getCurrentPosition(function (position) {
             getUserAddressBy(position.coords.latitude, position.coords.longitude)
         },
@@ -10,18 +9,20 @@
 
     function getUserAddressBy(lat, long) {
         var xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function () {
+        xhttp.onreadystatechange = function (results) {
             if (this.readyState == 4 && this.status == 200) {
                 var address = JSON.parse(this.responseText)
-                nextStep(address);
+                nextStep(address.results[0].formatted);
           
             }
         };
-        xhttp.open("GET", "https://maps.googleapis.com/maps/api/geocode/json?latlng="+lat+","+long+"&key=7fd4be117d8c45499762b2c94b1c20e2", true);
+          xhttp.open("GET", "https://api.opencagedata.com/geocode/v1/json?q="+ lat +","+long+"&key=7fd4be117d8c45499762b2c94b1c20e2", true);
         xhttp.send();
     }
 
 })();
-function nextStep(){
-    console.log(address.results[0].formatted_address)
+function nextStep(address){
+    console.log(address.split(", ")[3])
+
+    document.getElementById('location').innerHTML += "<h1>" + address.split(", ")[3] + "</h1>"
 }
