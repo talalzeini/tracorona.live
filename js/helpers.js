7c63e51573da128cbcaf9911ede815f4616e99a3
talalzeini <56060993+talalzeini@users.noreply.github.com>
2020-11-27 04:43:39 -0800

    added "updated ago" and fixed files issue

--- js/helpers.js
+++ js/helpers.js
@@ -5,6 +5,9 @@ function numberWithCommas(x) {
 var countriesURL = "https://corona.lmao.ninja/v3/covid-19/countries"
 var worldURL = "https://corona.lmao.ninja/v3/covid-19/all"
 var unitedStatesURL = 'https://corona.lmao.ninja/v2/states'
+
+
+
 function sortByCases(numberOfCountries, countryData){
         for (let i = 0; i < numberOfCountries; i++) {
             for (let j = 1; j < numberOfCountries-1; j++) {
@@ -18,7 +21,7 @@ function sortByCases(numberOfCountries, countryData){
 }
 function displayData(numberOfCountries, countryData){
     for(let i = 0; i < numberOfCountries-200; i++){
-        var countryDataSetup = "<tr id='underTable' class='rowColorChange'><td  style='text-align:left;cursor:pointer;' onclick='checkCountry(this.innerText)'>" + countryData[i].country  + "</td><td style='color:blue'>" + numberWithCommas(countryData[i].cases) + "</td><td style='color:red'>" + numberWithCommas(countryData[i].deaths) + "</td><td style='color:green;'>" + numberWithCommas(countryData[i].recovered) + "</td><td style='color:orange;'>" + numberWithCommas(countryData[i].critical) + "</td><td style='color:#fada5e;'>" + numberWithCommas(countryData[i].active)    + "</td></tr>"
+        var countryDataSetup = "<tr id='underTable' class='rowColorChange'><td class='countryButton' style='text-align:left;cursor:pointer;' onclick='checkCountry(this.innerText)'>" + countryData[i].country  + "</td><td style='color:blue'>" + numberWithCommas(countryData[i].cases) + "</td><td style='color:red'>" + numberWithCommas(countryData[i].deaths) + "</td><td style='color:green;'>" + numberWithCommas(countryData[i].recovered) + "</td><td style='color:orange;'>" + numberWithCommas(countryData[i].critical) + "</td><td style='color:#fada5e;'>" + numberWithCommas(countryData[i].active)    + "</td></tr>"
         document.getElementById('table').innerHTML += countryDataSetup
         if (i % 2 == 0){ // if even
             document.getElementsByClassName('rowColorChange')[i].style.backgroundColor = "#eee"
@@ -42,8 +45,40 @@ function checkCountry(country, statesTableHeader, countryData){
         })
     }
 }
+function timeDiffCalc(dateFuture, dateNow) {
+    let diffInMilliSeconds = Math.abs(dateFuture - dateNow) / 1000;
+
+    // calculate days
+    const days = Math.floor(diffInMilliSeconds / 86400);
+    diffInMilliSeconds -= days * 86400;
+    console.log('calculated days', days);
+
+    // calculate hours
+    const hours = Math.floor(diffInMilliSeconds / 3600) % 24;
+    diffInMilliSeconds -= hours * 3600;
+    console.log('calculated hours', hours);
+
+    // calculate minutes
+    const minutes = Math.floor(diffInMilliSeconds / 60) % 60;
+    diffInMilliSeconds -= minutes * 60;
+    console.log('minutes', minutes);
+
+    let difference = '';
+    if (days > 0) {
+      difference += (days === 1) ? `${days} day, ` : `${days} days, `;
+    }
+
+
+
+    difference += (minutes === 0 || hours === 1) ? `${minutes} minutes` : `${minutes} minutes`; 
+
+    return difference;
+  }
+
 function getWorldData(){
     $.get(worldURL, function(worldData){
+        var updatedAgo =  timeDiffCalc(new Date(), new Date(worldData.updated));
+        document.getElementById('updatedAgo').innerHTML = "Updated "  + updatedAgo + " ago"
         document.getElementById('worldData').innerHTML += "<td style='text-align:left;'> WORLD </td><td style='color:blue'>" + numberWithCommas(worldData.cases) + "</td><td style='color:red'>" + numberWithCommas(worldData.deaths) + "</td><td style='color:green;'>" + numberWithCommas(worldData.recovered) + "</td><td style='color:orange;'>" + numberWithCommas(worldData.critical) + "</td><td style='color:#fada5e;'>" + numberWithCommas(worldData.active) + "</td>"
         document.getElementById('totalCases').innerText = numberWithCommas(worldData.cases)
         document.getElementById('totalDeaths').innerText = numberWithCommas(worldData.deaths)
